259942125bff90648e12ac9d03a83f61cf1d6bf8
Joseph Mattello <mail@joemattiello.com>
2022-12-29 01:10:42 -0500

    provenance: custom play loader name
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- src/vulkan/Loader.cpp
+++ src/vulkan/Loader.cpp
@@ -74,6 +74,7 @@ void CLoader::LoadLibrary()
 	
 	std::vector<const char*> libPaths;
 #ifdef __APPLE__
+	libPaths.push_back("@executable_path/Frameworks/libMoltenVK_Play.dylib");
 	libPaths.push_back("@executable_path/../Resources/libMoltenVK.dylib");
 	libPaths.push_back("@executable_path/Frameworks/libMoltenVK.dylib");
 	libPaths.push_back("@executable_path/libMoltenVK.dylib");
