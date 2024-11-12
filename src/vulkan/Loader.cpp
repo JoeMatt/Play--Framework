dbdcbf2f4632f4034af5eaa42106f53feb06502b
Joe Mattiello <mail@joemattiello.com>
2024-11-12 15:04:59 -0500

    Update Loader.cpp for iOS/macOS frameworks
    Adds support for MoltenVK linked as a .framework, usually from a .xcframework.

--- src/vulkan/Loader.cpp
+++ src/vulkan/Loader.cpp
@@ -76,6 +76,8 @@ void CLoader::LoadLibrary()
 #ifdef __APPLE__
 	libPaths.push_back("@executable_path/../Resources/libMoltenVK.dylib");
 	libPaths.push_back("@executable_path/libMoltenVK.dylib");
+	libPaths.push_back("@executable_path/Contents/MoltekVK.framework/MoltenVK");
+	libPaths.push_back("@executable_path/Frameworks/MoltekVK.framework/MoltenVK");
 #else
 	libPaths.push_back("libvulkan.so");
 	libPaths.push_back("libvulkan.so.1");
